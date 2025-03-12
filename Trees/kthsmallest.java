643c7daa6ce4a641c1100efcb23e8267a88a8fe3
Shagun Gupta <127201788+Shagungupta7@users.noreply.github.com>
2025-03-12 23:18:31 +0530

    Create kthsmallest.java

---
+++ Trees/kthsmallest.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        helper(root, minHeap, k);
+        int ans = 0;
+        for(int i = 0; i < k; i++){
+            ans = minHeap.poll();
+        }
+        return ans;
+    }
+
+    void helper(TreeNode root, PriorityQueue<Integer> minHeap, int k){
+        if(root == null) return;
+
+        helper(root.left, minHeap, k);
+
+        minHeap.offer(root.val);
+
+        helper(root.right, minHeap, k);
+
+        return;
+    }
+}
