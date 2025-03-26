742741fc3df58f050738e193a6ad27eff4a48b7f
Shagun Gupta <127201788+Shagungupta7@users.noreply.github.com>
2025-03-26 18:36:06 +0530

    Update maximalSquare.java

--- DynamicProgramming/maximalSquare.java
+++ DynamicProgramming/maximalSquare.java
@@ -21,3 +21,26 @@ class Solution {
     }
 
 }
+
+//bottom-up
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int n = matrix.length, m = matrix[0].length;
+        if(matrix == null || n == 0 || m == 0) return 0;
+
+        int[][] dp = new int[n+1][m+1];
+
+        int len = 0;
+        for(int r = 1; r <= n; r++){
+            for(int c = 1; c <= m; c++){
+                if(matrix[r-1][c-1] == '1'){
+                    dp[r][c] = Math.min(dp[r-1][c-1], Math.min(dp[r-1][c], dp[r][c-1]))+1;
+                    len = Math.max(len, dp[r][c]);
+                }
+            }
+        }
+
+        return len*len;
+    }
+
+}
