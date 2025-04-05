d9a348d5ff731572e8375b380a3cc6d7614f3797
Shagun Gupta <127201788+Shagungupta7@users.noreply.github.com>
2025-04-05 17:11:13 +0530

    Create removeOuterParentheses.java

---
+++ Strings/removeOuterParentheses.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+
+        for(char c : s.toCharArray()){
+            if(c == ')') count--;
+            if(count > 0) sb.append(c);
+            if(c == '(') count++;
+        }
+        return sb.toString();
+    }
+}
